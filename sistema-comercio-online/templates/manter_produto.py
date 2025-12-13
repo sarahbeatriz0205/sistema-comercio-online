c9a87003638feae2375f199d56613f290849f5c3
Sarah Beatriz <sarah.nascimento02052007@gmail.com>
2025-12-13 14:23:37 +0000

    reajustar preco feito

--- sistema-comercio-online/templates/manter_produto.py
+++ sistema-comercio-online/templates/manter_produto.py
@@ -1,16 +1,18 @@
 import streamlit as st
 import pandas as pd
 from views import View
+import time
 
 class ManterProdutoUI:
     def main():
         st.header("Cadastro de produtos")
 
-        tab1, tab2, tab3, tab4 = st.tabs(["Listar", "Inserir", "Atualizar", "Excluir"])
+        tab1, tab2, tab3, tab4, tab5 = st.tabs(["Listar", "Inserir", "Atualizar", "Excluir", "Reajustar preços"])
         with tab1: ManterProdutoUI.listar()
         with tab2: ManterProdutoUI.inserir()
         with tab3: ManterProdutoUI.atualizar()
         with tab4: ManterProdutoUI.excluir()
+        with tab5: ManterProdutoUI.reajustar_preco()
 
     def listar():
         produtos = View.produto_listar()
@@ -28,8 +30,13 @@ class ManterProdutoUI:
         estoque = st.number_input("Estoque atual", value=0)
         categoria = st.selectbox("Categoria", View.categoria_listar())
         if len(View.categoria_listar()) > 0 and st.button("Inserir"): 
-            View.produto_inserir(id, descricao, preco, estoque, categoria.get_id())
-            st.success("Produto adicionado com sucesso!")
+            try:
+                View.produto_inserir(id, descricao, preco, estoque, categoria.get_id())
+                st.success("Produto adicionado com sucesso!")
+                time.sleep(1)
+                st.rerun()
+            except Exception as erro:
+                st.error(f"{erro}")
 
     def atualizar():
         produtos = View.produto_listar()
@@ -40,11 +47,15 @@ class ManterProdutoUI:
             preco = st.number_input("Novo preço", min_value=0.0, value=op.get_preco())
             estoque = st.number_input("Novo estoque", min_value=0, value=op.get_estoque())
             categorias = st.selectbox("Nova categoria", View.categoria_listar())
-            if len(View.categoria_listar()) > 0 and  st.button("Atualizar"):
-                id = op.get_idProduto()
-                View.produto_atualizar(id, descricao, preco, estoque, categorias.get_id())
-                st.success("Produto atualizado com sucesso!")
-                st.rerun()
+            if len(View.categoria_listar()) > 0 and st.button("Atualizar"):
+                try:
+                    id = op.get_idProduto()
+                    View.produto_atualizar(id, descricao, preco, estoque, categorias.get_id())
+                    st.success("Produto atualizado com sucesso!")
+                    time.sleep(1)
+                    st.rerun()
+                except Exception as erro:
+                    st.error(f"{erro}")
     
     def excluir():
         produtos = View.produto_listar()
@@ -60,7 +71,23 @@ class ManterProdutoUI:
                     idCategoria = op.get_idCategoria()
                     View.produto_excluir(id, descricao, preco, estoque, idCategoria)
                     st.success("Produto excluído com sucesso!")
+                    time.sleep(1)
                     st.rerun()
     
-    #def reajustar_preco():
-        #produtos = View.produto_listar()
+    def reajustar_preco():
+        produtos = View.produto_listar()
+        if len(produtos) == 0: st.write("Nenhum produto até o momento")
+        else:
+            op = st.selectbox("Selecione o produto a ser reajustado", produtos)
+            descricao = op.get_descricao()
+            estoque = op.get_estoque()
+            categorias = op.get_idCategoria()
+            novo_preco = st.number_input("Reajuste", min_value=0.0, value=op.get_preco())
+            if st.button("Confirmar alteração"):
+                id = op.get_idProduto()
+                View.produto_atualizar(id, descricao, novo_preco, estoque, categorias)
+                st.success("Preço reajustado com sucesso!")
+                time.sleep(1)
+                st.rerun()
+
+
